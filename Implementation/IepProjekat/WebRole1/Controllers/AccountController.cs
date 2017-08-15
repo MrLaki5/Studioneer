48d065e3164013a2246d7b458741c84e614ac99d
MrLaki5 <mimar1207@gmail.com>
2017-08-15 15:26:01 +0200

    Login/register
    dodati u navbar, controller account, view account

---
+++ Implementation/IepProjekat/WebRole1/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRole1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Acount
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+
+}
