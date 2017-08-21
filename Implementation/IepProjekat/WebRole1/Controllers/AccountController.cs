60df2e236c43bd0fa7ff78b51991b4c8b83bce2f
MrLaki5 <mimar1207@gmail.com>
2017-08-21 17:51:34 +0200

    POSLEDNJI COMMIT POGRESNOG PROJEKTA
    i hope

--- Implementation/IepProjekat/WebRole1/Controllers/AccountController.cs
+++ Implementation/IepProjekat/WebRole1/Controllers/AccountController.cs
@@ -30,6 +30,8 @@ namespace WebRole1.Controllers
         [HttpGet]
         public ActionResult EditInfo()
         {
+            if (Session["username"]==null)
+                return RedirectToAction("Login", "Account");
             return View();
         }
 
@@ -61,6 +63,7 @@ namespace WebRole1.Controllers
                 User user = users.First();
                 Session["username"] = user.Username;
                 Session["type"] = user.Type;
+                Session["token"] = user.Balans.ToString();
                 return Content("Success", "text/plain");
             }
 
@@ -116,28 +119,56 @@ namespace WebRole1.Controllers
         }
 
         [HttpPost]
-        public ActionResult EditInfo(string email, string name, string password, string cpassword)
+        public ActionResult EditInfo(string type, string newval, string cpassword)
         {
-            if (String.IsNullOrEmpty(email))
-                return Content("Enter email", "text/plain");
-            if (String.IsNullOrEmpty(name))
-                return Content("Enter username", "text/plain");
-            if (String.IsNullOrEmpty(password))
-                return Content("Enter password", "text/plain");
-            if (String.IsNullOrEmpty(cpassword))
-                return Content("Enter confirmation of password", "text/plain");
-            if (string.Compare(password, cpassword) != 0)
-                return Content("Passwords don't match", "text/plain");
-            try
-            {
-                var emailChecked = new System.Net.Mail.MailAddress(email);
+            if (Session["username"] == null)
+                return RedirectToAction("Login", "Account");
+            string name = Session["username"].ToString();
+            string password1 = FormsAuthentication.HashPasswordForStoringInConfigFile(cpassword, "SHA1");
+            var users = from m in db.User select m;
+            users = users.Where(s => s.Username.Equals(name)).Where(s => s.Password.Equals(password1));
+            if (!users.Any()) {
+                return Content("Wrong password", "text/plain");
             }
-            catch
-            {
-                return Content("Email is not valid", "text/plain");
+            User user = users.First();
+            switch (type) {
+                case "email":
+                    if (String.IsNullOrEmpty(newval))
+                        return Content("Enter email", "text/plain");
+                    try
+                    {
+                        var emailChecked = new System.Net.Mail.MailAddress(newval);
+                    }
+                    catch
+                    {
+                        return Content("Email is not valid", "text/plain");
+                    }
+                    users = users.Where(s => s.Mail.Equals(newval));
+                    if (users.Any())
+                    {
+                        return Content("Email is taken", "text/plain");
+                    }
+                    user.Mail = newval;
+                    db.SaveChanges();
+                    break;
+                case "password":
+                    if (String.IsNullOrEmpty(newval))
+                        return Content("Enter password", "text/plain");
+                    user.Password= FormsAuthentication.HashPasswordForStoringInConfigFile(newval, "SHA1");
+                    break;
+                case "username":
+                    if (String.IsNullOrEmpty(newval))
+                        return Content("Enter username", "text/plain");
+                    users = users.Where(s => s.Username.Equals(newval));
+                    if (users.Any())
+                    {
+                        return Content("Username is taken", "text/plain");
+                    }
+                    user.Username = newval;
+                    db.SaveChanges();
+                    break;
             }
-
-            return View();
+            return Content("Succesfully changed", "text/plain");
         }  
 
 
