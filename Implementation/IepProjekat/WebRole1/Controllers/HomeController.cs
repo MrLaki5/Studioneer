4e8daa2c9277969ca11f63854dcce91183109b5a
MrLaki5 <mimar1207@gmail.com>
2017-08-23 18:00:23 +0200

    question create started

--- Implementation/IepProjekat/WebRole1/Controllers/HomeController.cs
+++ Implementation/IepProjekat/WebRole1/Controllers/HomeController.cs
@@ -20,24 +20,5 @@ namespace WebRole1.Controllers
             }
             return RedirectToAction("Info", "Account");
         }
-
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
