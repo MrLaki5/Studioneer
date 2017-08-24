139007d3f8b25f34b2c18b789a06a9558a33afbe
MrLaki5 <mimar1207@gmail.com>
2017-08-24 13:25:29 +0200

    Qustion create sibanja bez

--- Implementation/IepProjekat/WebRole1/Controllers/QuestionController.cs
+++ Implementation/IepProjekat/WebRole1/Controllers/QuestionController.cs
@@ -1,21 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebRole1.Models;
 
 namespace WebRole1.Controllers
 {
     public class QuestionController : Controller
     {
-        // GET: Question
-        public ActionResult Index()
+
+        private Model1 db = new Model1();
+
+        [HttpGet]
+        public ActionResult Create()
         {
-            return View();
+            var parameters = from m in db.Parameters select m;
+            if (parameters.Any()) {
+                Parameter par = parameters.First();
+                ViewBag.k = par.AnswerNumber;
+                return View();
+            }
+            return RedirectToAction("Logout", "Account");
         }
 
-        public ActionResult Upload()
+        [HttpPost]
+        public ActionResult Create(HttpPostedFileBase file)
         {
+            if (Session["type"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            if (Session["type"].ToString() != "Professor")
+            {
+                return RedirectToAction("Logout", "Account");
+            }
+
+            if (file != null) {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(Server.MapPath("~/Images"), pic);
+                while (!System.IO.File.Exists(path))
+                {
+                    pic = "1" + pic;
+                    path = System.IO.Path.Combine(Server.MapPath("~/Images"), pic);
+                }
+                // file is uploaded
+                file.SaveAs(path);
+
+            }
+
+
+
+            string name = Request.Form["naslov"];
             return View();
         }
     }
