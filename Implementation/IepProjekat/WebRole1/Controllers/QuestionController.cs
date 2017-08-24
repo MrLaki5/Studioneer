922eb82165a42b938fc5679791989c1fd84649c6
MrLaki5 <mimar1207@gmail.com>
2017-08-24 17:17:01 +0200

    Controller question create 1.0

--- Implementation/IepProjekat/WebRole1/Controllers/QuestionController.cs
+++ Implementation/IepProjekat/WebRole1/Controllers/QuestionController.cs
@@ -29,36 +29,100 @@ namespace WebRole1.Controllers
         [HttpPost]
         public ActionResult Create(HttpPostedFileBase file)
         {
-            /*if (Session["type"] == null)
+            if (Session["type"] == null)
             {
                 return RedirectToAction("Login", "Account");
             }
             if (Session["type"].ToString() != "Professor")
             {
                 return RedirectToAction("Logout", "Account");
-            }*/
+            }
 
             var parameters = from m in db.Parameters select m;
-            if (parameters.Any())
-            {
+            if (parameters.Any()){
                 Parameter par = parameters.First();
                 ViewBag.k = par.AnswerNumber;
             }
 
-            if (Request.Form["title"] == null){
+            if (Request.Form["ttitle"] == null){
                 return RedirectToAction("Create", "Question");
             }
-            string title = Request.Form["title"];
+            string title = Request.Form["ttitle"];
             if (title.Length > 20){
                 ViewBag.err = "Title length max 20 characters";
                 return View();
             }
-            if (Request.Form["title"] == null)
-            {
+            if (title == ""){
+                ViewBag.err = "Title required";
+                return View();
+            }
+            if (Request.Form["text"] == null){
                 return RedirectToAction("Create", "Question");
+            }
+            string text = Request.Form["text"];
+            if (text.Length > 200)
+            {
+                ViewBag.err = "Text length max 200 characters";
+                return View();
+            }
+            if (text == "")
+            {
+                ViewBag.err = "Text required";
+                return View();
             }
 
 
+            if (Request.Form["radio202"] == null)
+            {
+                return RedirectToAction("Create", "Question");
+            }
+            string correctAnswer = Request.Form["radio202"];
+
+            string email = Session["email"].ToString();
+            var users = from m in db.Users select m;
+            users = users.Where(s => s.Mail.Equals(email));
+            int idU;
+            if (users.Any())
+            {
+                User user = users.First();
+                idU = user.IdU;
+            }
+            else
+            {
+                return RedirectToAction("Logout", "Home");
+            }
+
+
+            Answer[] ans = new Answer[ViewBag.k];
+
+            for (int i = 0; i < ViewBag.k; i++)
+            {
+                ans[i] = new Answer();
+                if (Request.Form["answer " + i] == null)
+                {
+                    return View("Create", "Question");
+                }
+                if (Request.Form["answer " + i] == "")
+                {
+                    ViewBag.err = "All answers are required";
+                    return View("Create", "Question");
+                }
+                string temp = Request.Form["answer " + i];
+                ans[i].Number = i + 1;
+                ans[i].Text = temp;
+                ans[i].Tag = (char)(i + 65) + "";
+                if (correctAnswer == "" + i)
+                {
+                    ans[i].IsCorrect = 1;
+                }
+                else
+                {
+                    ans[i].IsCorrect = 0;
+                }
+            }
+
+            string path = "";
+            bool succ = false;
             if (file != null) {
                 string exten = Path.GetExtension(file.FileName).ToLower();
                 if ((exten != ".jpg") && (exten != ".jpeg") && (exten != ".png")){       //bad type of file check
@@ -72,7 +136,7 @@ namespace WebRole1.Controllers
                 string pic = System.IO.Path.GetFileName(file.FileName);
                 int count = 0;
                 string pom = FormsAuthentication.HashPasswordForStoringInConfigFile(pic, "SHA1");
-                string path = System.IO.Path.Combine(Server.MapPath("~/Images"), pom+pic);
+                path = System.IO.Path.Combine(Server.MapPath("~/Images"), pom+pic);
                 while (System.IO.File.Exists(path))
                 {
                     pom = count+"" + pic;
@@ -81,12 +145,32 @@ namespace WebRole1.Controllers
                     count++;
                 }
                 pic = pom;
-                // file is uploaded
-                file.SaveAs(path);
+                file.SaveAs(path);                                        // file is uploaded
+                succ = true;
+            }
+            
+
+            Question question = new Question();
+            question.Title = title;
+            question.Text = text;
+            question.IdU = idU;
+            question.IsClone = 0;
+            //question.IsLocked =                   dodati is locked
+            question.CreationTime = DateTime.UtcNow;
+            if (succ){
+                question.Image = path;
             }
 
+            
 
-
+            db.Questions.Add(question);
+            db.SaveChanges();
+            for (int i=0;i< ViewBag.k; i++){
+                ans[i].IdP = question.IdP;
+                db.Answers.Add(ans[i]);
+            }
+            db.SaveChanges();
+        
             return View();
         }
     }
